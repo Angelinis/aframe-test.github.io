be7e00ac288d130401628568476ebbc106b2295b
Angelo C <angmogollon@gmail.com>
2023-06-29 19:06:54 -0300

    Adding raycaster to Oculus control

---
+++ javascript-scripts/index.js
@@ -0,0 +1,26 @@
+/* global AFRAME */
+const videosArray = [
+  "#video01",
+  "#video02",
+  "#video03",
+  "#video04"
+]
+
+function changeVideo(){
+  let videoSource = document.querySelector("#myVideo").getAttribute("src");
+  videoSource = videosArray.indexOf(videoSource);
+  document.querySelector("#myVideo").setAttribute("src", videosArray[videoSource + 1] || videosArray[0]);
+}
+
+document.querySelector("#changeVideo").addEventListener("click", () => changeVideo())
+
+const rightController = document.querySelector("#rightController");
+
+rightController.addEventListener("raycaster-intersected", function (event) {
+  const intersectedElement = event.detail.intersectedElems[0];
+  if (intersectedElement.classList.contains("clickable")) {
+    if(intersectedElement.id === "changeVideo"){
+      changeVideo();
+    }         
+}
+})
