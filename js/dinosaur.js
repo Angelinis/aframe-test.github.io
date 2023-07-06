3e6e2ad2adfeee363461fab54540062186378831
Angelo C <angmogollon@gmail.com>
2023-07-06 18:47:28 -0300

    Adding dinosaur changes

---
+++ js/dinosaur.js
@@ -0,0 +1,23 @@
+let score = 0
+
+
+
+AFRAME.registerComponent('raycaster-listener', {
+  init: function () {
+    this.el.addEventListener('raycaster-intersected', evt => {
+      this.raycaster = evt.detail.el;
+    });
+    this.el.addEventListener('raycaster-intersected-cleared', evt => {
+      this.raycaster = null;
+    });
+  },
+  tick: function () {
+    if (!this.raycaster) { return; }  // Not intersecting.
+    let intersection = this.raycaster.components.raycaster.getIntersection(this.el);
+    if (!intersection) { return; } // Not intersecting
+    const intersectedObject = intersection.object;
+    if (intersectedObject){
+      document.querySelector("[text]").setAttribute("value", `${++score} dinosaurios cazados`);
+    }
+  }
+});
